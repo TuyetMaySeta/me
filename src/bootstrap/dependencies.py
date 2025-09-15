140bc73716902f0aaf8f9d512d171895713e36a6
TuyetMaySeta <may.nguyen@seta-international.vn>
2025-09-15 13:43:26 +0700

    Update dependencies.py

--- src/bootstrap/dependencies.py
+++ src/bootstrap/dependencies.py
@@ -1,12 +1,11 @@
-
 from src.bootstrap.application_bootstrap import app_bootstrap
 
 
 def get_employee_controller():
-    """Dependency to get employee controller from bootstrap"""
+    """Dependency to get Employee controller from bootstrap"""
     return app_bootstrap.employee_controller
 
 
 def get_employee_related_controller():
-    """Dependency to get employee related controller from bootstrap"""
+    """Dependency to get Employee related controller from bootstrap"""
     return app_bootstrap.employee_related_controller
