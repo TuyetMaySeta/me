3401d92172cb2e7c6166b7766cbd9e96273cbb90
TuyetMaySeta <may.nguyen@seta-international.vn>
2025-09-23 07:10:51 +0700

    Update enums.py

--- src/core/enums.py
+++ src/core/enums.py
@@ -1,9 +1,10 @@
-# src/core/enums.py
 from enum import Enum
+
 
 class GenderEnum(str, Enum):
     MALE = "Male"
     FEMALE = "Female"
+
 
 class ProficiencyEnum(str, Enum):
     NATIVE = "Native"
@@ -11,12 +12,14 @@ class ProficiencyEnum(str, Enum):
     INTERMEDIATE = "Intermediate"
     BASIC = "Basic"
 
+
 class SkillCategoryEnum(str, Enum):
     PROGRAMMING_LANGUAGE = "Programming Language"
     DATABASE = "Database"
     FRAMEWORK = "Framework"
     TOOL = "Tool"
     HARDWARE = "Hardware"
+
 
 class SoftSkillEnum(str, Enum):
     COMMUNICATION = "Communication"
@@ -28,18 +31,25 @@ class SoftSkillEnum(str, Enum):
     ADAPTABILITY = "Adaptability"
     OTHER = "Other"
 
+
 class MaritalStatusEnum(str, Enum):
     SINGLE = "Single"
     MARRIED = "Married"
     DIVORCED = "Divorced"
     WIDOWED = "Widowed"
 
+
 class EmployeeStatusEnum(str, Enum):
     ACTIVE = "Active"
     ON_LEAVE = "On Leave"
     RESIGNED = "Resigned"
 
+
 class DraftStatusEnum(str, Enum):
     DRAFT = "Draft"
     APPROVED = "Approved"
     REJECTED = "Rejected"
+    
+class SessionProviderEnum(str, Enum):
+    LOCAL = "Local"
+    MICROSOFT = "Microsoft"
