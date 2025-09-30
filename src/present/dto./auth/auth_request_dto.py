b2c65c50f55ac1042d9ec68b3447358c9390374b
TuyetMaySeta <may.nguyen@seta-international.vn>
2025-09-30 10:38:06 +0700

    Update auth_request_dto.py

--- src/present/dto./auth/auth_request_dto.py
+++ src/present/dto./auth/auth_request_dto.py
@@ -1,13 +1,12 @@
 # src/present/dto/auth/auth_request_dto.py
 from typing import Optional
 
-from pydantic import BaseModel, Field
-
+from pydantic import BaseModel, Field,EmailStr
 
 class LoginRequestDTO(BaseModel):
     """Login request with employee ID and password"""
 
-    employee_id: str = Field(..., description="Employee ID or email")
+    employee_id: int = Field(..., description="Employee ID")
     password: str = Field(..., min_length=1, description="Employee password")
     remember_me: Optional[bool] = Field(
         False, description="Remember me for extended session"
@@ -30,3 +29,17 @@ class RefreshTokenRequestDTO(BaseModel):
 
     class Config:
         json_schema_extra = {"example": {"refresh_token": "refresh_token_here"}}
+
+class VerifyOldPasswordDTO(BaseModel):
+    """"Verify old password"""
+    old_password: str = Field(...,min_length =6, description = "Seta123@")
+    class Config:
+        json_chema_extra = {"example": {"old_password": "Seta123@"}}
+
+class CreatOTP(BaseModel):
+    email: EmailStr = Field(..., description ="a@example.com")
+    class Config:
+        json_chema_extra = {"example": {"email": "example@gmail.com"}}
+class VerifyOTP(CreatOTP):
+    otp_code: int = Field(..., ge=100000, le=999999, description="OTP code 6 chữ số")
+
