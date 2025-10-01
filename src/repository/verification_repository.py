90b9fa27038a7c75a4913842d1c4cb2c1e5179ee
TuyetMaySeta <may.nguyen@seta-international.vn>
2025-10-01 19:37:52 +0700

    Update verification_repository.py

--- src/repository/verification_repository.py
+++ src/repository/verification_repository.py
@@ -1,35 +1,45 @@
 import logging
 from datetime import datetime, timezone
 from typing import Optional
+
 from sqlalchemy.orm import Session
+
 from src.core.enums.verification import VerificationTypeEnum
 from src.core.models.verification_code import VerificationCode
 from src.repository.base_repository import BaseRepository
 
 logger = logging.getLogger(__name__)
 
+
 class VerificationRepository(BaseRepository[VerificationCode]):
     def __init__(self, db: Session):
         super().__init__(db, VerificationCode)
 
-    def create_verification(self,verification:VerificationCode) -> VerificationCode:
+    def create_verification(self, verification: VerificationCode) -> VerificationCode:
         self.db.add(verification)
         self.db.commit()
         self.db.refresh(verification)
         return verification
-    def get_otp_verification(self, employee_id:int, verification_type: VerificationTypeEnum) -> Optional[VerificationCode]:
+
+    def get_active_otp(
+        self, employee_id: int, verification_type: VerificationTypeEnum
+    ) -> Optional[VerificationCode]:
         """Check if there's an active OTP of employee_id"""
         now = datetime.now(timezone.utc)
         return (
-            self.db.query(VerificationCode).filter(
+            self.db.query(VerificationCode)
+            .filter(
                 VerificationCode.employee_id == employee_id,
                 VerificationCode.type == verification_type,
-                VerificationCode.expires_at > now
+                VerificationCode.expires_at > now,
             )
             .first()
         )
-    def get_valid_otp(self,employee_id:int, code:str, verification_type: VerificationTypeEnum) -> Optional[VerificationCode]:
-        """Get valid OTP """
+
+    def get_valid_otp(
+        self, employee_id: int, code: str, verification_type: VerificationTypeEnum
+    ) -> Optional[VerificationCode]:
+        """Get valid OTP"""
         now = datetime.now(timezone.utc)
         return (
             self.db.query(VerificationCode)
@@ -40,16 +50,16 @@ class VerificationRepository(BaseRepository[VerificationCode]):
                 VerificationCode.expires_at > now,
             )
             .first()
-        )  
-    
+        )
+
     def mark_as_used(self, verification_id: int) -> None:
         """Mark OTP as used by setting expiration to now"""
-        verification = self.db.query(VerificationCode).filter(
-            VerificationCode.id == verification_id
-        ).first()
-        
+        verification = (
+            self.db.query(VerificationCode)
+            .filter(VerificationCode.id == verification_id)
+            .first()
+        )
+
         if verification:
             verification.expires_at = datetime.now(timezone.utc)
-            self.db.commit()
-
-
+            self.db.commit()  
